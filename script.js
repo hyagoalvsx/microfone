9de3558a074c02599a4e5f1c1e54688a3352658e
Hyago Alves <hyago1305@gmail.com>
2026-04-01 09:55:17 -0400

    microfone funcionando

---
+++ script.js
@@ -0,0 +1,36 @@
+let player;
+
+function onYouTubeIframeAPIReady(){
+    player = new YT.Player ('player', {
+        height: '360',
+        width: '640',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event){
+    event.target.playVideo();
+}
+
+async function startMic(){
+    const stream = await navigator.mediaDevices.getUserMedia({audio: true})
+    const audioContext = new AudioContext();
+    const mic = audioContext.createMediaStreamSource(stream);
+    const analyser = audioContext.createAnalyser();
+
+    mic.connect(analyser);
+
+    const data = new Uint8Array(analyser.frequencyBinCount);
+
+    function checkVolume(){
+        analyser.getByteFrequencyData(data);
+        let volume = data.reduce((a,b) => a+b) / data.length;
+        let videoVolume = Math.min(volume * 2, 100);
+        player.setVolume(videoVolume);
+        requestAnimationFrame(checkVolume);
+    }
+    checkVolume();
+}
